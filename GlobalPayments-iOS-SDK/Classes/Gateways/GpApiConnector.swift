734c4a74cc18bde294c6f056a5cc422e8ca623e0
Alexandr Pavlov <alexandr.pavlov@outlook.com>
2020-09-28 14:15:13 +0300

    Retry get access token if existing token expired

--- GlobalPayments-iOS-SDK/Classes/Gateways/GpApiConnector.swift
+++ GlobalPayments-iOS-SDK/Classes/Gateways/GpApiConnector.swift
@@ -60,29 +60,46 @@ class GpApiConnector: RestGateway, PaymentGateway, IReportingService {
     func signOut() { }
 
     override func doTransaction(method: HTTPMethod,
-                                endpoint: String,
-                                data: String? = nil,
-                                queryStringParams: [String : String]? = nil,
-                                completion: @escaping (String?, Error?) -> Void) {
-        guard let accessToken = accessToken, !accessToken.isEmpty else {
-            signIn { success, error in
-                guard success else {
-                    completion(nil, error)
+                                    endpoint: String,
+                                    data: String? = nil,
+                                    queryStringParams: [String : String]? = nil,
+                                    completion: @escaping (String?, Error?) -> Void) {
+
+
+        verifyAuthentication { error in
+
+            if let gatewayError = error as? GatewayException,
+               gatewayError.responseCode == "NOT_AUTHENTICATED" {
+
+                self.verifyAuthentication { error in
+                    if let error = error {
+                        completion(nil, error)
+                        return
+                    }
+
+                    super.doTransaction(
+                        method: method,
+                        endpoint: endpoint,
+                        data: data,
+                        queryStringParams: queryStringParams,
+                        completion: completion
+                    )
                     return
                 }
-                super.doTransaction(method: method,
-                                    endpoint: endpoint,
-                                    data: data,
-                                    queryStringParams: queryStringParams,
-                                    completion: completion)
+
+            } else if let error = error {
+                completion(nil, error)
+                return
             }
-            return
+
+            super.doTransaction(
+                method: method,
+                endpoint: endpoint,
+                data: data,
+                queryStringParams: queryStringParams,
+                completion: completion
+            )
         }
-        super.doTransaction(method: method,
-                            endpoint: endpoint,
-                            data: data,
-                            queryStringParams: queryStringParams,
-                            completion: completion)
     }
 
     override func handleResponse(response: GatewayResponse?,
